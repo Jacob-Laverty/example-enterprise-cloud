86acb41a10ffdb44d761d24d90a42d0b2e09f3e3
Jacob Laverty <jacob.laverty@gmail.com>
2024-01-15 23:33:51 -0500

    Add a small test instance to the stack

--- packages/enterprise-cloud-lib/main.ts
+++ packages/enterprise-cloud-lib/main.ts
@@ -1,14 +1,28 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { Instance } from "@cdktf/provider-aws/lib/instance";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // define resources here
+    new AwsProvider(this, "example-enterprise-us-east-1", {
+      region: "us-east-1",
+      profile: "example-enterprise-admin"
+    })
+
+    new Instance(this, "example", {
+      ami: "ami-0005e0cfe09cc9050",
+      instanceType: "t2.micro",
+      tags: {
+        Name: "example"
+      }
+    })
   }
 }
 
 const app = new App();
-new MyStack(app, "foo");
+new MyStack(app, "Enterprise-Example");
 app.synth();
